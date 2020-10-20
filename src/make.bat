d9bc1c6a84003567069ee5972af920ae9ee344da
RuRuRu <rururutan+github@gmail.com>
2020-10-20 09:17:13 +0900

    make.bat: Fixed for latest MinGW

--- src/make.bat
+++ src/make.bat
@@ -1,1 +1,2 @@
-g++ -mno-cygwin -O2 -omdz80.exe mdZ80.cpp
+g++ -O2 -static -omdz80.exe mdZ80.cpp
+g++ -DMESSAGETYPE_JAPANESE -finput-charset=cp932 -fexec-charset=cp932 -O2 -static -omdz80j.exe mdZ80.cpp
